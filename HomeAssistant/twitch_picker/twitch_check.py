94d9d149fd6ec6d975f8770b1a98cc60e80636b3
afairclo <41074440+afairclo@users.noreply.github.com>
2022-12-17 13:37:33 -0500

    Update twitch_check.py
    Add condition for stuck 'playing' media_player status. If they are no longer streaming, JSON data will be empty; run pick script.

--- HomeAssistant/twitch_picker/twitch_check.py
+++ HomeAssistant/twitch_picker/twitch_check.py
@@ -14,6 +14,10 @@ def twitch_check(action=None, id=None):
     response = task.executor(requests.get, pickURL, headers=requestHeaders)
     pickJson = response.json()
     
+    if pickJson['data'] == None:
+        service.call("pyscript", "twitch_picker")
+        exit()
+    
     for pick_info in pickJson['data']:
         input_text.twitch_picker_title.set_value(pick_info['title']+" 🔴 "+str(pick_info['viewer_count']))
         if pick_info['game_name'] != input_text.twitch_picker_game:
